621efb20b4fcd2091db8388efac42f6379b4726d
Pavel Stančík <stancik@infowebsro.cz>
2021-05-02 10:02:35 +0200

    feature/List all secrets

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
     init: require('./lib/init'),
     setup: require('./lib/setup'),
+    listAll: require('./lib/listAll'),
     keyVault: require('./lib/key-vault')
 };
